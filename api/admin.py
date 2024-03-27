9e1953785f52aac5e888c854681dafa99e8895bb
Johnny <johnkcli@hotmail.com>
2024-03-27 16:01:19 +0000

    admin can filter to list only reported videos

--- api/admin.py
+++ api/admin.py
@@ -2,16 +2,48 @@ import datetime
 
 from django.contrib import admin
 from django.utils.safestring import mark_safe
+from django.utils.translation import gettext_lazy as _
 from firebase_admin import storage
 from firebase_admin.auth import delete_user
 
 from api.models import Video, User
 
 
+class ReportedVideoListFilter(admin.SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = _("Reported Videos")
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = "reported"
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        return [
+            (True, _("Reported")),
+        ]
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        if self.value():
+            return queryset.filter(reported_by__isnull=False)
+
+
 class VideoModelAdmin(admin.ModelAdmin):
     readonly_fields = ["creator", "display_video"]
     fields = ["creator", "display_video"]
     search_fields = ["id"]
+    list_filter = [ReportedVideoListFilter]
 
     def has_add_permission(self, request, obj=None):
         return False
