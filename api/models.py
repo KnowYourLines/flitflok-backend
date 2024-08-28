f61bb69c40d231bcd74a63d67ee243dbec4324fa
Johnny <johnkcli@hotmail.com>
2024-08-29 00:27:58 +0100

    add starring user to video

--- api/models.py
+++ api/models.py
@@ -32,6 +32,9 @@ class Video(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     uploaded_at = models.DateTimeField()
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    starring = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="videos_starring"
+    )
     location = models.PointField()
     thumbnail = models.URLField()
     hls = models.URLField()
